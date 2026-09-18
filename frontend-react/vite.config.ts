a40a5eb3f731a32aa8f977a3ae570d8ed46fc62d
Jeratos <ayushchouhan417@gmail.com>
2026-09-18 18:47:09 +0530

    frontend port change

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5175,          // any port you want
+    host: true,          // 👈 this makes it accessible on the network
+  },
 })
